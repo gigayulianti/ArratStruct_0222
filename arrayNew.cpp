c6013dba30eb7a224b66e21389286467a1130d8c
gigayulianti <yulianti2@gmail.com>
2026-06-13 17:54:43 +0700

    commit 3

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -28,4 +28,23 @@ int main() {
     // Menampilkan Satu per Satu
     cout << "Data Pertama = " << kamu[0] << endl;
     cout << "Data Kedua = " << kamu[1] << endl;
-    
+    cout << "Data Ketiga = " << kamu[2] << endl;
+    cout << "Data Keempat = " << kamu[3] << endl;
+    cout << endl;
+
+    //Mengisi dengan menggunakan perulangan for
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data index ke-" << i << " = ";
+        cin >> kamu[i];
+
+    }
+    cout << endl;
+    // Menampilkan dengan menggunakan perulangan for
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke-" << i+1 << " = " << kamu[i] << endl;
+    }
+
+
+}
