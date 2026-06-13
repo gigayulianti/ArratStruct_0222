084e5f036329bbbac65e85c4fb930e4c5292fbdd
gigayulianti <yulianti2@gmail.com>
2026-06-13 18:03:45 +0700

    commit 14

--- arrayStruct.cpp
+++ arrayStruct.cpp
@@ -11,3 +11,14 @@ struct Orang
     string nama;
     detailAlamat alamat;
     int umur;
+};
+int main() {
+    Orang mhs;
+    //Mengisi data
+    cout << "mengisi data" << endl;
+    cout << "Nama : " ;
+    getline(cin , mhs.nama);
+    cout << "Desa : " ;
+    getline(cin , mhs.alamat.desa);
+    cout << "Kota : " ;
+    
