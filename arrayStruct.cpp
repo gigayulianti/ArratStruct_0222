9363bac4d1581e27961a169f4c36dd5048e8270b
gigayulianti <yulianti2@gmail.com>
2026-06-13 17:57:29 +0700

    commit 6

--- arrayStruct.cpp
+++ arrayStruct.cpp
@@ -20,5 +20,14 @@ int main() {
     getline(cin , mhs.nama);
     cout << "Desa : " ;
     getline(cin , mhs.alamat.desa);
-    
+    cout << "Kota : " ;
+    getline(cin , mhs.alamat.kota);
+    cout << "Umur : " ;
+    cout << endl;
+    // tampilkan 
+    cout << "Menampilkan Data" << endl;
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Alamat : " << mhs.alamat.desa << endl;
+    cout << " Alamat : " << mhs.alamat.kota << endl;
+    cout << " Umur : " << mhs.umur << endl;
 }
